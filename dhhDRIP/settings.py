064d40de0be3d8231dae7d2d2d9dc4dadceaf9fb
manichaursia04 <manistorage14@gmail.com>
2026-09-16 10:30:15 +0530

    change mail setting

--- dhhDRIP/settings.py
+++ dhhDRIP/settings.py
@@ -139,8 +139,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("manistorage14@gamil.com")
+EMAIL_HOST_PASSWORD = os.environ.get("yghi tnty ipjf igkk")
 
-EMAIL_HOST_USER = "manistorage14@gamil.com"
-EMAIL_HOST_PASSWORD = "yghi tnty ipjf igkk"
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
